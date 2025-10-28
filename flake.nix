a363f5aaf6bc69654e172c9737a9b2ea245fca0c
Oliver Starup <Oliver@starup99.dk>
2025-10-28 11:55:20 +0100

    Added flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          # Packages to be installed in the environment
+          packages = with pkgs; [
+            python313Packages.flask
+          ];
+
+          # Shell hook for environment variables and aliases
+        };
+      }
+    );
+}
